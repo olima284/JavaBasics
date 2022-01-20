644a0c94c0ba003c7be495e8312c466faab0c891
olima284 <olivialima.student@careerdevs.com>
2022-01-20 18:53:21 -0500

    started ask the user cha

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -3,6 +3,12 @@ package com.careerdevs;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	String firstName = "Olivia";
+    String lastName = "Lima";
+    String fullName = firstName + " " + lastName;
+    }
+
+    public static void getFullName(String name){
+        System.out.println("Welcome + fullName");
     }
 }
---
+++ src/com/careerdevs/asktheuser/UserInterface.java
@@ -0,0 +1,4 @@
+package com.careerdevs.asktheuser;
+
+public class UserInterface {
+}
