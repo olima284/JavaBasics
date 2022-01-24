ca60fcf28f5ee2f8eed3c81fe847b83e75b3f1d3
olima284 <olivialima.student@careerdevs.com>
2022-01-24 16:12:47 -0500

    started ask the user cha

--- src/com/careerdevs/asktheuser/UserInterface.java
+++ src/com/careerdevs/asktheuser/UserInterface.java
@@ -1,4 +1,46 @@
 package com.careerdevs.asktheuser;
 
+import java.util.Scanner;
+
 public class UserInterface {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+//String--> scanner.nextString()
+//        System.out.println("name: ");
+//        String userName = scanner.nextLine();
+//        System.out.println("Hello " + userName);
+//Byte --> scanner.nextByte();
+//        System.out.println("age: ");
+//        Byte userAge = scanner.nextByte();
+//        System.out.println("How old are you " + userAge);
+        //
+//int --> scanner.nextInt();
+        // int userWeeklyViews = 4000
+        // scanner.nextInt();
+        //System.out.println("new users per week" + userWeeklyViews );
+
+////float --> scanner.nextFloat();
+//        System.out.print("Enter the amount: ");
+//        float users = scanner.nextFloat();
+//        System.out.println("You are getting close " + users );
+//long --> scanner.nextLong();
+//        long usersInTotal = 777_85_099L;
+//        System.out.println("You are Gold level With  " + usersInTotal + " users");
+//double --> scanner.nextDouble();
+//        double balance;
+//        System.out.println("Enter the Total Balance: ");
+//        balance = scanner.nextDouble();
+////
+//boolean --> scanner.nextBoolean();
+//        System.out.println("Are you over 30?-");
+//        boolean bn = scanner.nextBoolean();
+//        if(bn == true) {
+//            System.out.println( "You are over 30");
+//        }
+
+    }
 }
+//Notes
+//main/enter(psvm/enter):mainclass
+//(sout):(Systems.out.println())
