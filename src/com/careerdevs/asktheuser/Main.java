4d385f3d20d5c93a8063202a97388d51aebd6488
olima284 <olivialima.student@careerdevs.com>
2022-02-03 17:02:41 -0500

    done

--- src/com/careerdevs/asktheuser/Main.java
+++ src/com/careerdevs/asktheuser/Main.java
@@ -1,60 +1,87 @@
 package com.careerdevs.asktheuser;
 
+import java.net.PortUnreachableException;
+import java.security.PublicKey;
 import java.util.Scanner;
 
 public class Main {
+    public static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-        askVideoUser();
-    }
-
-    public static void askVideoUser() {
-
-    Scanner scan = new Scanner(System.in);
-    System.out.println("What your userName: ");
-    String userName = scan.nextLine();
-    System.out.println("This User Name IS " + userName);
-
-    System.out.println("What is your age: ");
-    byte userAge = scan.nextByte();
-    System.out.println(userName + ":" + userAge);
-
-    System.out.println("How many user viewed your video: ");
-    int weeklyUserViews = scan.nextInt();
-    System.out.println(userName +"has views:"+ weeklyUserViews + "views" );
-
-    System.out.println("How many new users: ");
-    short newWeeklyUsers = scan.nextShort();
-    System.out.println(userName +"Has new views:"+ newWeeklyUsers  + "views" );
-;
-
-    System.out.println("What is your year total views: ");
-    long totalViewsPerYear = scan.nextLong();
-    System.out.println(userName +"total this year is :"+ totalViewsPerYear  + "views" );
-
-    System.out.println("What is your monthly total earning: ");
-    float monthlyEarning= scan.nextFloat();
-    System.out.println( userName + "earned" + monthlyEarning);
-
-    System.out.println("How much did earn this year");
-    double yearlyEarningPercent= scan.nextDouble();
-    System.out.println(yearlyEarningPercent );
-
-
-    System.out.println(totalViewsPerYear > 2000l);
-    boolean giveUserAStar = totalViewsPerYear > 2000L;
-   if (giveUserAStar) {
-       System.out.println( "YOU HAVE A STAR!");
-   } else {
-       System.out.println("YOU DO NOT GET A STAR!");
-   }
-
-
-
-
-
-
+        String userName = name();
+        byte userAge = age();
+        int weeklyUserViews = weeklyViews();
+        short newWeeklyUsers = newUsers();
+        long totalViewsPerYear = perYear();
+        float monthlyEarning = monthlyMoney();
+        double yearlyEarningPercent = yearlyPercent();
+        star(totalViewsPerYear);
 
     }
+
+    public static String name() {
+        System.out.println("What your userName: ");
+        String userName = scan.nextLine();
+        System.out.println("This User Name IS " + userName);
+        return userName;
+
+    }
+
+    public static byte age() {
+        System.out.println("What is your age: ");
+        byte userAge = scan.nextByte();
+        System.out.println("Your age is: " + userAge);
+        return userAge;
+    }
+
+    public static int weeklyViews() {
+        System.out.println("How many user viewed your video: ");
+        int weeklyUserViews = scan.nextInt();
+        System.out.println("Your weekly views are: " + weeklyUserViews);
+        return weeklyUserViews;
+
+    }
+
+
+    public static short newUsers() {
+        System.out.println("How many new users: ");
+        short newWeeklyUsers = scan.nextShort();
+        System.out.println("Has new users: " + newWeeklyUsers);
+        return newWeeklyUsers;
+    }
+
+    public static long perYear() {
+        System.out.println("What is your year total views: ");
+        long totalViewsPerYear = scan.nextLong();
+        System.out.println( "total this year is :" + totalViewsPerYear + "views");
+        return totalViewsPerYear;
+    }
+
+    public static float monthlyMoney() {
+        System.out.println("What is your monthly total earning: ");
+        float monthlyEarning = scan.nextFloat();
+        System.out.println("earned" + monthlyEarning);
+        return monthlyEarning;
+    }
+
+    public static double yearlyPercent() {
+        System.out.println("How much did earn this year");
+        double yearlyEarningPercent = scan.nextDouble();
+        System.out.println(yearlyEarningPercent);
+        return yearlyEarningPercent;
+    }
+
+    public static void star(long totalViewsPerYear) {
+        System.out.println(totalViewsPerYear > 2000l);
+        boolean giveUserAStar = totalViewsPerYear > 2000L;
+        if (giveUserAStar) {
+            System.out.println("YOU HAVE A STAR!");
+        } else {
+            System.out.println("YOU DO NOT GET A STAR!");
+        }
+
+    }
+
 }
 
 //    public String userName;
