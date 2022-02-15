05268a7e655592aa78b7136838222e525e770527
olima284 <olivialima.student@careerdevs.com>
2022-02-14 20:46:11 -0500

    worked on asktheuser

---
+++ src/com/careerdevs/recursionpractice/CountDown.java
@@ -0,0 +1,31 @@
+package com.careerdevs.recursionpractice;
+
+public class CountDown {
+
+    public static void main(String[] args) {
+        int countDownFrom;
+      //  iterative(countDownFrom : 5);
+    }
+    private static void iterative (int countDownFrom){
+        //option 1
+//      for (int i = 0; i < countDownFrom; i++){
+//      System.out.println(i);
+  //  }
+    //opt 2
+//      for (int i = countDownFrom; i > 0; i--){
+//      System.out.println(i);
+  //  }
+    //opt3
+//       int i = countDownFrom;
+//        while (i <= 1){
+//        System.out.println(i);
+//      i--;
+//      }
+
+//      //opt
+//        while (countDownFrom > 0){
+//               System.out.println(countDownFrom);
+//                --countDownFrom
+                }
+}
+
