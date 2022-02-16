a4d1c87d752326b5631023764b506ea81501721a
olima284 <olivialima.student@careerdevs.com>
2022-02-15 19:03:11 -0500

    Intro To Recursion finished

--- src/com/careerdevs/recursionpractice/CountDown.java
+++ src/com/careerdevs/recursionpractice/CountDown.java
@@ -3,12 +3,12 @@ package com.careerdevs.recursionpractice;
 public class CountDown {
 
     public static void main(String[] args) {
-        int countDownFrom;
-      //  iterative(countDownFrom : 5);
+
+      iterative(5);
     }
     private static void iterative (int countDownFrom){
         //option 1
-     for (int i = 0; i < countDownFrom; i++){
+     for (int i = 0; i < countDownFrom; i++) {
      System.out.println(i);
   }
     //opt 2
@@ -28,4 +28,35 @@ public class CountDown {
 //                --countDownFrom
                 }
 }
+
+
+
+//Define recursion
+
+
+//Recursion is the technique of making a function call itself.
+//This technique provides a way to break complicated problems down into simple problems which are easier to solve
+
+//https://www.geeksforgeeks.org/recursion-in-java/
+//The idea is to represent a problem in terms of one or more smaller problems,
+// and add one or more base conditions that stop the recursion. For example:
+//we compute factorial n if we know factorial of (n-1). The base case for factorial would be n = 0. We return 1 when n = 0.
+
+
+
+//Intro To Recursion
+//Write a function that recursively finds the sum of the first n natural numbers.
+//https://edabit.com/challenge/nMbE2g7MB5yFcTzoB
+//   public class Challenge {
+//   public static int sum(int n) {
+//        if(n==0)
+//            return 0;
+//        else if(n==1)
+//            return 1;
+//        else
+//            return n+sum(n-1);
+
+//    }
+
+
 
